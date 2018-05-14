2f4e1e8e19eb96947fb932a3557e43dbc7c5d5d8
JorgeManuelMolina <36421174+JorgeManuelMolina@users.noreply.github.com>
2018-05-14 09:33:00 +0200

    Populated en proceso

--- Item 4/Sample-Project-1.14/src/main/java/security/Authority.java
+++ Item 4/Sample-Project-1.14/src/main/java/security/Authority.java
@@ -38,7 +38,9 @@ public class Authority implements GrantedAuthority {
 	// Values -----------------------------------------------------------------
 
 	public static final String	ADMIN		= "ADMIN";
-	public static final String	CUSTOMER	= "CUSTOMER";
+	public static final String	MODERATOR	= "MODERATOR";
+	public static final String	USER		= "USER";
+	public static final String	BUSINESS	= "BUSINESS";
 
 	// Attributes -------------------------------------------------------------
 
@@ -46,7 +48,7 @@ public class Authority implements GrantedAuthority {
 
 
 	@NotBlank
-	@Pattern(regexp = "^" + Authority.ADMIN + "|" + Authority.CUSTOMER + "$")
+	@Pattern(regexp = "^" + Authority.ADMIN + "|" + Authority.MODERATOR + "|" + Authority.USER + "|" + Authority.BUSINESS + "$")
 	@Override
 	public String getAuthority() {
 		return this.authority;
@@ -67,7 +69,15 @@ public class Authority implements GrantedAuthority {
 		result.add(authority);
 
 		authority = new Authority();
-		authority.setAuthority(Authority.CUSTOMER);
+		authority.setAuthority(Authority.MODERATOR);
+		result.add(authority);
+
+		authority = new Authority();
+		authority.setAuthority(Authority.USER);
+		result.add(authority);
+
+		authority = new Authority();
+		authority.setAuthority(Authority.BUSINESS);
 		result.add(authority);
 
 		return result;
