948f83763441ba932d39494b62aed8e9f006812e
Guillermo Bandrés Catalina <gbandres98@gmail.com>
2018-06-06 18:30:19 +0200

    Removed the most evil line ever.

--- Item 4/Sample-Project-1.14/src/main/java/controllers/user/PaymentUserController.java
+++ Item 4/Sample-Project-1.14/src/main/java/controllers/user/PaymentUserController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import services.ActorService;
 import services.AuctionAdvertisementService;
@@ -41,7 +40,6 @@ import forms.PaymentResponse;
 
 @Controller
 @RequestMapping("/user/payment")
-@EnableWebMvc
 public class PaymentUserController {
 
 	@Autowired
